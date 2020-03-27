d1b1473bce5eaf86d08657f757b208303e1b3db9
The Pandemic <gailbail.e.y591.52@gmail.com>
2020-03-27 19:01:06 +0900

    Update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TELEGRAM_TOKEN = os.environ.get('1014840884:AAHRl3QtKjOjTAz9BIgPV-4XWXA95Y5s3bE', '')
-CHAT_ID = os.environ.get('CHAT_ID', '')
+CHAT_ID = os.environ.get('1146016071', '')
 
 if not TELEGRAM_TOKEN or not CHAT_ID:
-  raise Exception('1014840884:AAHRl3QtKjOjTAz9BIgPV-4XWXA95Y5s3bE, CHAT_ID 확인필요')
+  raise Exception('1014840884:AAHRl3QtKjOjTAz9BIgPV-4XWXA95Y5s3bE, 1146016071')
--- noti.py
+++ noti.py
@@ -1,8 +1,9 @@
-from config import TELEGRAM_TOKEN, CHAT_ID
+from config
+import TELEGRAM_TOKEN, CHAT_ID
 import requests as rq
 import telegram 
 
 bot = telegram.Bot(token=1014840884:AAHRl3QtKjOjTAz9BIgPV-4XWXA95Y5s3bE)
 
 def send(t):
-  bot.sendMessage(CHAT_ID, t, parse_mode=telegram.ParseMode.HTML)
+  bot.sendMessage(1146016071, t, parse_mode=telegram.ParseMode.HTML)
