787ebc934fa6168dd8290c05f4263be5dd290a0b
The Pandemic <gailbail.e.y591.52@gmail.com>
2020-03-27 21:58:17 +0900

    update news.py

--- news.py
+++ news.py
@@ -105,6 +105,7 @@ for i in range(10):
     
     daum_news = daum_new_titles[i] + '\n\n' + daum_new_links[i]
     news(daum_news)
+    
 """
 ===보낼 링크===
  ['https://m.news.naver.com/read.nhn?mode=LSD&mid=sec&sid1=101&oid=008&aid=0004349743', 'http://it.chosun.com/site/data/html_dir/2020/01/31/2020013103216.html', 'https://m.news.naver.com/read.nhn?mode=LSD&mid=sec&sid1=101&oid=031&aid=0000523810', 'https://m.news.naver.com/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0011371561', 'http://www.fintechpost.co.kr/news/articleView.html?idxno=100097'] 
