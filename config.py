699d05a69255e12dde444d7bc45f12a50690ca0a
The Pandemic <gailbail.e.y591.52@gmail.com>
2020-03-27 19:24:47 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TELEGRAM_TOKEN = os.environ.get('1014840884:AAHRl3QtKjOjTAz9BIgPV-4XWXA95Y5s3bE', '')
-CHAT_ID = os.environ.get('1146016071', '')
+CHAT_ID = os.environ.get('-1001212798809', '')
 
 if not TELEGRAM_TOKEN or not CHAT_ID:
-  raise Exception('1014840884:AAHRl3QtKjOjTAz9BIgPV-4XWXA95Y5s3bE, 1146016071')
+  raise Exception('1014840884:AAHRl3QtKjOjTAz9BIgPV-4XWXA95Y5s3bE, -1001212798809')
